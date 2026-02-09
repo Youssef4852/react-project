c2259c08ad2664ba78591d3aefc14515fa722dfb
Youssef Osama <youssef6yassen7@gmail.com>
2026-02-09 17:38:26 +0200

    Fix Faq accordion behavior and toggle items

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Faq.css";
-import FaqItem from "../../components/FaqItem";
 
 function Faq() {
+  const [openIndex, setOpenIndex] = useState(0)
+  const faqItems = [
+    {
+      title: "How It Work This Application In Broswer?",
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis dolore cupiditate hic deleniti, iste molestiae commodi expedita error",
+    },
+    {
+      title: "Is React fast?",
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis dolore cupiditate hic deleniti, iste molestiae commodi expedita error",
+    },
+    {
+      title: "What is JSX?",
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis dolore cupiditate hic deleniti, iste molestiae commodi expedita error",
+    },
+    {
+      title: "What is Next Js?",
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis dolore cupiditate hic deleniti, iste molestiae commodi expedita error",
+    },
+  ];
   return (
     <>
       <section className="faq" id="faq">
@@ -10,30 +28,17 @@ function Faq() {
           Let's Ask A Question And The Answer In <span>FAQ</span>
         </h2>
         <div className="container">
-          <FaqItem
-            title="How It Work This Application In Broswer?"
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit
-          quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis
-          dolore cupiditate hic deleniti, iste molestiae commodi expedita error."
-          />
-          <FaqItem
-            title="How It Work This Application In Broswer?"
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit
-          quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis
-          dolore cupiditate hic deleniti, iste molestiae commodi expedita error."
-          />
-          <FaqItem
-            title="How It Work This Application In Broswer?"
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit
-          quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis
-          dolore cupiditate hic deleniti, iste molestiae commodi expedita error."
-          />
-          <FaqItem
-            title="How It Work This Application In Broswer?"
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit
-          quos neque cumque ullam quam dolor culpa at quibusdam quo. Reiciendis
-          dolore cupiditate hic deleniti, iste molestiae commodi expedita error."
-          />
+          {faqItems.map((faq, index) => {
+            return (
+              <div key={faq.title} className={`question ${openIndex === index ? "open" : ''}`} onClick={() => setOpenIndex(index)}>
+                <h3>{faq.title}</h3>
+                <i className={`bx bx-${openIndex === index ? 'minus' : 'plus'}`}></i>
+                <div className="question-text">
+                  <p>{faq.text}</p>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </section>
     </>
